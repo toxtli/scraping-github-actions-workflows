17b9144550084502c7b410a298dbff674fd168cb
Carlos Toxtli <ctoxtli@gmail.com>
2021-05-19 23:50:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import json
+import requests
+from datetime import date
 
+url = 'https://httpbin.org/get'
+response = requests.get(request_url)
+data = response.json()
+
+json.dump(data, open('data/dump_{}.csv'.format(date.today().strftime('%Y%m%d'), 'w'))
